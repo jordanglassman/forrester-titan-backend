dd6a4f69dbe2171b12bd6c8cd06f3ef903988f24
Jordan Glassman <jglassman@cloudbees.com>
2018-06-27 08:29:11 -0400

    ANALYTICS-1926 bad

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        // insert bad code here
+        insert bad code here
     }
 }
